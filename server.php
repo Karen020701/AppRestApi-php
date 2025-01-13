21ca687bef25e105a31512854f61e8ffae1f8bf6
Karen020701 <karenchicaiza989@gmail.com>
2025-01-12 23:32:14 -0500

    first commit

---
+++ server.php
@@ -0,0 +1,15 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+   
+    echo json_encode(["message" => "Hola Mundo con RestApi en lenguaje php"]);
+} else {
+    
+    http_response_code(405);
+    echo json_encode(["message" => "Método no permitido"]);
+}
+?>
